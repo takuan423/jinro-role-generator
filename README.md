2dba754bfcbb3eea8f85274fb718c6f2bb3945b8
takuya.kikuchi <k.takuya423@gmail.com>
2024-07-13 20:20:54 +0900

    :wrench: chore: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # jinro-role-generator
 人狼の役割決めのときに役に立つアプリ
+
+## 開発環境の立ち上げ方
+
+```bash
+pnpm install
+```
+
+```bash
+pnpm dev
+```
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  srcDir: 'src/',
+  ssr: true,
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt', '@pinia/nuxt', '@nuxt/test-utils/module'],
+  shadcn: {
+    prefix: '',
+    componentDir: './src/components/ui/',
+  },
+  pinia: {
+    storesDirs: ['./src/store'],
+  },
+  compatibilityDate: '2024-07-04',
+});
